61f324e59b032212a971dac513608dd859a88716
ArthurSimplicio <105257098+ArthurSimplicio@users.noreply.github.com>
2023-04-21 20:48:17 -0300

    Add files via upload
    Atualizado

---
+++ Curso JS/Mammoth Interactive/interactions.js
@@ -0,0 +1,85 @@
+function buttonFunction0(){
+    document.getElementById('pText').style.color= 'orange';
+    document.getElementById('pText').style.fontSize = '50px';
+    document.getElementById('pText').style.fontFamily = 'Impact';
+}
+
+function buttonFunction1(){
+    document.getElementById('pText').style.color= 'blue';
+    document.getElementById('pText').style.fontSize = '90px';
+    document.getElementById('pText').style.fontFamily = 'Courier';
+}
+
+/*function FirstFunction(){
+    alert("Congratulations, you made your first interaction!")
+}*/
+
+/*function FunctionOne(){
+    alert("1");
+}
+
+function FunctionTwo(){
+    alert("2");
+}
+
+function FunctionThree(){
+    alert("3");
+}
+
+function FunctionFour(){
+    alert("4");
+}*/
+/*var Number = 52;
+var String = "AWEOSME! "
+
+var numberArray = [5,4,3,2,1,"M"];
+var stringArray = ["Arthur", "Carlos", "Carla"];
+var spc = " ";
+
+Object
+var car = {name: "Wolksvagen", range: 230, unity: "km", color: "Black", topSpeed: 300, sunroof: "NO"}
+function ChangeText(){
+    Number = Number + 10;
+document.getElementById('pText').innerHTML = car.name + spc + car.range + car.unity + spc + car.color + spc + car.topSpeed + spc + car.sunroof;
+}*/
+
+/*function onClickedButton(){
+    ChangeText();
+}
+
+function ChangeText(){
+    var userInputText = document.getElementById('ChangeUserText').value;
+
+    document.getElementById('pText').innerHTML = userInputText;
+}
+*/
+
+/*global var
+    var someNumber = 12;
+    var changeVariable = 0;
+function describe(){
+    local var
+    var someNumber = document.getElementById('ChangeUserText').value;
+var changeVariable = 0;
+
+if (someNumber != 12){
+    changeVariable = 1;
+}
+document.getElementById('pText').innerHTML = changeVariable;
+
+function describe(){
+    var someNumber = document.getElementById('ChangeUserText').value;
+    var changeVariable = 0;
+
+if (someNumber == 13){
+    changeVariable = 1;
+}else if(someNumber == 12){
+    changeVariable = 2;
+}else if(someNumber == 11){
+    changeVariable = 3;
+}else{
+    changeVariable = 4;
+}
+document.getElementById('pText').innerHTML = changeVariable;
+}
+}*/
